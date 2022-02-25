852b2028c8c33c119a4703d442c37215ac5ee0a6
Ivan Moya <ivanlmoya@gmail.com>
2022-02-24 22:05:58 -0300

    Se suben 3 ejercicios javascript

---
+++ practica3.js
@@ -0,0 +1,24 @@
+let adivinar = 0;
+let a =Math.round( Math.random()*9 +1);
+let puntaje = 0;
+let vidas = 3;
+
+console.log(a);
+
+alert("Bienvenido al juego de adivina el numero (1-10)")
+
+do{
+    adivinar = prompt("Ingrese un Numero :");
+    if (a == adivinar) {
+        alert("Adivinaste +1pt")
+        puntaje++
+        a =Math.round( Math.random()*9 +1);
+        console.log(a);
+    } else {
+        vidas--
+        console.log(vidas);
+    }
+}while(vidas > 0)
+
+alert(`Su puntaje fue de ${puntaje} , si quiere jugar nuevamente presione F5`); 
+
---
+++ practica4.js
@@ -0,0 +1,22 @@
+do {
+    valor = prompt("Ingrese una tabla de multiplicar : (1-100)")
+        for (i = valor; i < 100; i++) {
+            console.log(`tablas de multiplicar de ${i}`);
+            if(i == 69){
+                console.log("( ͡° ͜ʖ ͡°)");
+                console.log("escriba salir , para terminar")
+                break
+            }
+            else{
+            do{
+            for (j = 1; j < 10; j++) {
+                resultado = i * j;
+                console.log(`${i} x ${j} = ${resultado}`);
+            }
+            }while(j<10)
+            console.log("escriba salir , para terminar")
+            break
+            }
+        }
+}
+while (valor != "salir")
